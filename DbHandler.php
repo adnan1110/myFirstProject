d6dffa866e1520f9c7018d0f877df2b9cd0cb25b
Adnan AlSannaa <alsannaa@colorado.edu>
2014-08-17 13:52:00 +0300

    added user register and password

--- DbHandler.php
+++ DbHandler.php
@@ -11,7 +11,56 @@ Class DbHandler {
 		
 		$this->conn = $db->connect();
 	}
-
+	
+	/**
+     * Generating random Unique MD5 String for user Api key
+     */
+    private function generateApiKey() {
+        return md5(uniqid(rand(), true));
+    }
+	
+	public function createAccount($name, $email, $password){
+		require_once 'PassHash.php';
+		$response = array();
+		
+		if(!$this->isAccountExists($email)){
+			
+			$password_hash = PassHash::hash($password);
+			
+			$api_key = $this->generateApiKey();
+			
+			$params = array();
+			$params[0] = $name; 
+			$params[1] = $email; 
+			$params[2] = $password_hash;
+			$params[3] = $api_key; 
+			$params[4] = 1; 
+			
+			$result = pg_query_params($this->conn,"INSERT INTO accounts 
+			(name, email, password_hash, api_key, status) values 
+			($1, $2, $3, $4, $5);", $params);
+			
+			if(!$result){
+				return 1; 
+			}
+			
+			return 0;  
+		}
+		return 2; 
+	}
+	
+	private function isAccountExists($email) {
+		$params = array();
+		$params["email"] = $email;
+        $result = pg_query_params($this->conn,"SELECT id from accounts WHERE email = $1", $params);
+        if($result == false){
+			echo 'error occured' . pg_last_error($this->conn);
+			return NULL;
+		} else{ 
+			return pg_num_rows($result) > 0; 
+        }
+    }
+	
 	public function getAllAccounts(){
 	
 		$result = pg_query($this->conn,"SELECT * FROM accounts");
--- index.php
+++ index.php
@@ -18,20 +18,89 @@ function echoResponse($status_code, $response) {
 	echo json_encode ( $response );
 }
 
+function validateEmail($email) {
+    $app = \Slim\Slim::getInstance();
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $response["error"] = true;
+        $response["message"] = 'Email address is not valid';
+        echoResponse(400, $response);
+        $app->stop();
+    }
+}
 
+function verifyRequiredParams($required_fields) {
+    $error = false;
+    $error_fields = "";
+    $request_params = array();
+    $request_params = $_REQUEST;
+    // Handling PUT request params
+    if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+        $app = \Slim\Slim::getInstance();
+        parse_str($app->request()->getBody(), $request_params);
+    }
+    foreach ($required_fields as $field) {
+        if (!isset($request_params[$field]) || strlen(trim($request_params[$field])) <= 0) {
+            $error = true;
+            $error_fields .= $field . ', ';
+        }
+    }
+ 
+    if ($error) {
+        // Required field(s) are missing or empty
+        // echo error json and stop the app
+        $response = array();
+        $app = \Slim\Slim::getInstance();
+        $response["error"] = true;
+        $response["message"] = 'Required field(s) ' . substr($error_fields, 0, -2) . ' is missing or empty';
+        echoRespnse(400, $response);
+        $app->stop();
+    }
+}
 
 
 $app->get ( '/test', function (){
 	$response = array ();
 		
 	$response ["error"] = false;
-	$response ["message"] = "it works!!!";
 	
-	
-	
+	$db = new DbHandler ();
+	$result = $db->isAccountExists();
+	if($result)
+		$response ["message"] = "it works!!!" . $result;
+	else 
+		$response ["message"] = "it works!!! it didn't find it " . $result;
 	echoResponse( 200, $response );
 });
 
+$app->post('/register', function() use ($app){
+
+	verifyRequiredParams(array('name', 'email', 'password'));
+	$response = array();
+	
+	$name = $app->request->post('name');
+	$email = $app->request->post('email');
+	$password = $app->request->post('password');
+	
+	validateEmail($email);
+	
+	 $db = new DbHandler();
+     $result = $db->createAccount($name, $email, $password);
+
+	if ($result == 0) {
+		$response["error"] = false;
+		$response["message"] = "You are successfully registered";
+		echoResponse(201, $response);
+	} else if ($result == 1) {
+		$response["error"] = true;
+		$response["message"] = "Oops! An error occurred while registereing";
+		echoRespnse(200, $response);
+	} else if ($result == 2) {
+		$response["error"] = true;
+		$response["message"] = "Sorry, this email already existed";
+		echoResponse(200, $response);
+	}
+
+});
 
 $app->get ( '/databasetest', function (){
 	$response = array ();
